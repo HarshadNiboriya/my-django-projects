18fde5e58df72f9aa4d17cc82b16d18a2eaf55e6
Harshad-Niboriya <niboriyaharshad@gmail.com>
2026-09-21 13:50:08 +0530

    middleware- 19 Sept

---
+++ Django-middleware/sos/ors/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('test/', views.test_ors),
+    path('welcome/', views.welcome),
+    path('',views.welcome),
+    path('signup/', views.user_signup),
+    path('signin/', views.user_signin),
+    path('logout/', views.user_logout),
+    path('testlist/', views.test_list),
+    path('list/', views.user_list),
+    path('delete/<int:id>/', views.delete_user),
+    path('save/', views.user_save),
+    path('edit/<int:id>/',views.edit_user),
+]
