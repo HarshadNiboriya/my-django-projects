81954f049002dc34d51e95bf16724b86bc8ecafb
Harshad-Niboriya <niboriyaharshad@gmail.com>
2026-09-24 15:07:41 +0530

    23 Sept-simplified

--- django-crud/sos/ors/views.py
+++ django-crud/sos/ors/views.py
@@ -1,10 +1,10 @@
-from operator import index
-
+from django.contrib.sessions.models import Session
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .service.user_service import UserService
 from .utility.data_validator import DataValidator
+
 
 def user_signup_validate(request):
     input_error = {}
@@ -29,6 +29,7 @@ def user_signup_validate(request):
         input_error['error'] = True
     return input_error
 
+
 def user_signin_validate(request):
     input_error = {}
     input_error['error'] = False
@@ -40,8 +41,14 @@ def user_signin_validate(request):
         input_error['error'] = True
     return input_error
 
+
+def test_ors(request):
+    return HttpResponse('<h1>test ors app</h1>')
+
+
 def welcome(request):
-    return render(request,'welcome.html')
+    return render(request, 'welcome.html')
+
 
 def user_signup(request):
     form = {}
@@ -49,9 +56,12 @@ def user_signup(request):
     form['error'] = False
     form['input_error'] = {}
 
+    if request.method == "GET":
+        return render(request, 'registration.html', {'form': form})
+
     if request.method == "POST":
 
-        if request.POST.get('operation', '') == "signUp":
+        if request.POST.get('operation', '') == "signup":
             form['first_name'] = request.POST.get('firstName')
             form['last_name'] = request.POST.get('lastName')
             form['login_id'] = request.POST.get('loginId')
@@ -61,48 +71,61 @@ def user_signup(request):
 
             form['input_error'] = user_signup_validate(request)
 
-            if not form['input_error']['error']:
-                try:
-                    UserService().add(form)
-                    form['message'] = 'User Registration Successfully...!!!'
-                    form['error'] = False
-                except Exception as e:
-                    form['message'] = str(e)
-                    form['error'] = True
+            if form['input_error']['error']:
+                return render(request, 'registration.html', {'form': form})
+
+            try:
+                UserService().add(form)
+                form['message'] = 'User Registration Successfully...!!!'
+                form['error'] = False
+            except Exception as e:
+                form['message'] = str(e)
+                form['error'] = True
+            return render(request, 'registration.html', {'form': form})
 
         if request.POST.get('operation', '') == "reset":
             return redirect('/ors/signup/')
 
-    return render(request, 'registration.html', {'form': form})
+
 def user_signin(request):
     form = {}
     form['message'] = ''
     form['error'] = False
     form['input_error'] = {}
+
+    if request.method == "GET":
+        return render(request, 'login.html', {'form': form})
+
     if request.method == "POST":
-        form = {}
-        form['login_id'] = request.POST.get('loginId')
-        form['password'] = request.POST.get('password')
 
-        form['input_error'] = user_signin_validate(request)
+        if request.POST.get('operation', '') == "signIn":
+            form['login_id'] = request.POST.get('loginId')
+            form['password'] = request.POST.get('password')
 
-        if not form['input_error']['error']:
-            service = UserService()
-            user_data = service.authenticate(form['login_id'], form['password'])
+            form['input_error'] = user_signin_validate(request)
 
-            if len(user_data) > 0:
-                request.session['first_name'] = user_data[0].get('first_name')
+            if form['input_error']['error']:
+                return render(request, 'login.html', {'form': form})
+
+            user_data = UserService().authenticate(form['login_id'], form['password'])
+
+            if user_data:
+                request.session['first_na me'] = user_data[0].get('first_name')
                 return redirect('/ors/welcome/')
             else:
                 form['message'] = 'Login ID & Password Invalid'
                 form['error'] = True
 
-    return render(request, 'login.html', {'form': form})
+            return render(request, 'login.html', {'form': form})
+
+        if request.POST.get('operation', '') == "signUp":
+            return redirect('/ors/signup/')
 
 
 def user_logout(request):
     request.session['first_name'] = None
     return redirect('/ors/signin/')
+
 
 def test_list(request):
     list = [
@@ -112,13 +135,19 @@ def test_list(request):
         {"id": 4, "first_name": "Neha", "last_name": "Singh", "email": "neha@gmail.com", "password": "neha123"},
         {"id": 5, "first_name": "Rohit", "last_name": "Gupta", "email": "rohit@gmail.com", "password": "rohit123"}
     ]
-    return render(request,'test_list.html',{'list':list})
+    return render(request, "test_list.html", {"list": list})
 
 
 def user_list(request):
     form = {}
     form['page_no'] = 1
     form['page_size'] = 5
+    form['list'] = []
+
+    if request.method == "GET":
+        form['list'] = UserService().search(form)
+        form['index'] = (form['page_no'] - 1) * form['page_size']
+        return render(request, "user_list.html", {"form": form})
 
     if request.method == "POST":
         if request.POST['operation'] == "next":
@@ -133,16 +162,16 @@ def user_list(request):
             form['page_no'] = 1
             form['first_name'] = request.POST.get('firstName')
 
-    service = UserService()
-    list = service.search(form)
-    index = (form['page_no'] - 1) * 5
-    return render(request, "user_list.html", {"list": list, 'page_no': form['page_no'], 'index': index})
+        form['list'] = UserService().search(form)
+        form['index'] = (form['page_no'] - 1) * form['page_size']
+        return render(request, "user_list.html", {"form": form})
 
 
 def delete_user(request, id=0):
     service = UserService()
-    list = service.delete(id)
-    return redirect('/ors/list/')
+    service.delete(id)
+    return redirect("/ors/list/")
+
 
 def user_save(request):
     if request.method == "POST":
@@ -164,10 +193,12 @@ def user_save(request):
 
     return render(request, 'user.html')
 
+
 def edit_user(request, id=0):
     service = UserService()
     user_data = service.get(id)
-    return render(request,'user.html',{'data':user_data[0]})
+    return render(request, 'user.html', {'data': user_data[0]})
+
 
 def create_session(request):
     request.session['name'] = 'Admin'
@@ -198,6 +229,3 @@ def get_cookies(request):
     value = request.COOKIES.get('name')
     html = "<h3><center> value = {} </center></h3>".format(value)
     return HttpResponse(html)
-
-
-
